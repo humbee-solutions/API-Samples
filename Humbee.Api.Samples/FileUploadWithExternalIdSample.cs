208e8f2f52cc5a858c6a56a42fee71f2d57fa54d
Ralf Bönning <ralf.boenning@humbee.de>
2021-06-14 14:39:35 +0200

    Sample for file upload/download with foreign key.

---
+++ Humbee.Api.Samples/FileUploadWithExternalIdSample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Humbee.Api.Samples.Models;
+
+namespace Humbee.Api.Samples
+{
+    public class FileUploadWithExternalIdSample
+    {
+        public static async Task UploadDownloadAndDeleteFileAsync(HttpClient httpClient)
+        {
+            const string foreignKey = "abcdef-1234-ghi-567";
+            const string foreignKeyPool = "TestSystem 47b";
+            const string foreignNamespace = "Foobar-Inc-MyCoolApp";
+
+            // Upload File
+            await using var fileStream = new FileStream(".\\Data\\SimplePdf.pdf", FileMode.Open);
+
+            var formContent = new MultipartFormDataContent
+            {
+                {new StringContent(foreignNamespace), "ExternalReferenceNamespace"},
+                {new StringContent(foreignKeyPool), "ExternalReferencePoolId"},
+                {new StringContent(foreignKey), "ExternalReferenceId"},
+                {new StreamContent(fileStream), "file", "SimplePdf.pdf"}
+            };
+
+            var response = await httpClient.PostAsync("/api/documents", formContent);
+            response.EnsureSuccessStatusCode();
+            var creationResult = await response.Content.ReadFromJsonAsync<CreationResultModel>();
+            var humbeeDocumentId = creationResult?.Id;
+
+            Console.WriteLine($"Document created with id = {humbeeDocumentId}");
+
+            // Lookup Document by foreign key
+            response = await httpClient.GetAsync($"/api/documents/namespace/{foreignNamespace}/pool/{foreignKeyPool}/key/{foreignKey}");
+            response.EnsureSuccessStatusCode();
+
+            // Download file by foreign key
+            response = await httpClient.GetAsync($"/api/documents/namespace/{foreignNamespace}/pool/{foreignKeyPool}/key/{foreignKey}/file");
+            response.EnsureSuccessStatusCode();
+            var fileData = await response.Content.ReadAsByteArrayAsync();
+
+            Console.WriteLine($"Downloaded file data has got {fileData.Length} bytes.");
+
+
+            // Delete file
+            response = await httpClient.DeleteAsync("/api/documents/" + humbeeDocumentId);
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine($"Document {humbeeDocumentId} has been deleted.");
+        }
+    }
+}
--- Humbee.Api.Samples/Program.cs
+++ Humbee.Api.Samples/Program.cs
@@ -10,7 +10,7 @@ namespace Humbee.Api.Samples
     public class Program
     {
         private const string ApiBaseUrl = "https://cloud.humbee.de/";
-        
+
         public static async Task Main(string[] args)
         {
             var userEmail = args[0];
@@ -27,6 +27,7 @@ namespace Humbee.Api.Samples
 
             await ProfileSample.ShowProfileAsync(httpClient);
             await FileUploadDownloadDeleteSample.UploadDownloadAndDeleteFileAsync(httpClient);
+            await FileUploadWithExternalIdSample.UploadDownloadAndDeleteFileAsync(httpClient);
         }
 
         private static async Task<HttpClient> GetHumbeeApiHttpClientAsync(string userName, string userPassword, string clientId, string clientSecret, string tenantId)
